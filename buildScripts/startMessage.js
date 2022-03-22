644a4e21f28dbc867040b9efeae054b7d6fa2eca
Angie Todd <a.m.todd999@gmail.com>
2022-03-22 09:29:30 -0400

    initial commit

---
+++ buildScripts/startMessage.js
@@ -0,0 +1,3 @@
+import chalk from "chalk";
+//chalk is a library that allows you to select the color to display in the console message
+console.log(chalk.green('Starting this app in dev mode...')); // eslint-disable-line no-console
